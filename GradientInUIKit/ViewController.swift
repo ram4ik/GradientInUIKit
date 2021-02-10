d3f3a5ee5ec04605c8881f0b5fe17380aeecc963
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2021-02-10 12:48:30 +0200

    with color location

--- GradientInUIKit/ViewController.swift
+++ GradientInUIKit/ViewController.swift
@@ -19,10 +19,19 @@ class ViewController: UIViewController {
         
         let gradientLayer = CAGradientLayer()
         gradientLayer.frame = view.bounds
-        gradientLayer.colors = [UIColor.red.cgColor, UIColor.blue.cgColor]
+        gradientLayer.colors = [
+            UIColor.red.cgColor,
+            UIColor.blue.cgColor,
+            UIColor.yellow.cgColor,
+            UIColor.orange.cgColor,
+            UIColor.purple.cgColor,
+            UIColor.systemPink.cgColor
+        ]
         
-        gradientLayer.startPoint = CGPoint(x: 0.0, y: 0.5)
-        gradientLayer.endPoint = CGPoint(x: 1.0, y: 0.5)
+        gradientLayer.locations = [0.0, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
+        
+        gradientLayer.startPoint = CGPoint(x: 0.0, y: 0.0)
+        gradientLayer.endPoint = CGPoint(x: 1.0, y: 1.0)
         
         view.layer.addSublayer(gradientLayer)
     }
