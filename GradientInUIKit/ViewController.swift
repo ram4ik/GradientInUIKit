0c0347cad93c61917c8df800af05458f47c971db
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2021-02-10 12:41:52 +0200

    init

--- GradientInUIKit/ViewController.swift
+++ GradientInUIKit/ViewController.swift
@@ -13,7 +13,15 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = view.bounds
+        gradientLayer.colors = [UIColor.red.cgColor, UIColor.blue.cgColor]
+        view.layer.addSublayer(gradientLayer)
+    }
 
 }
 
