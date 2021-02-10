21cee35ebcadefdaf7a899313bdb811e328550f5
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2021-02-10 12:36:56 +0200

    Initial Commit

---
+++ GradientInUIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GradientInUIKit
+//
+//  Created by ramil on 10.02.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
